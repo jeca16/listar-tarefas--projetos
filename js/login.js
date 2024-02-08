e568a5f33fd597901c3e6c13e45aa133c3365044
JESSICA PEREIRA DOS SANTOS <23111734@CFP127ALUNO.LOCAL>
2024-02-08 12:13:26 -0300

    adicionar js finalizado e segunda pagina finalizada

---
+++ js/login.js
@@ -0,0 +1,25 @@
+const button = document.getElementById('login');
+
+async function validarLogin() {
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
+
+    if (email === '' || senha === '') {
+        alert('preencha os campos corretamente')
+    } else {
+        const users = await fetch('http://localhost:5080/usuario')
+        const listUsers = await users.json()
+
+        listUsers.forEach((user) => {
+            if (email === user.email && senha === user.senha) {
+                alert('Usuario logado com sucesso !!!')
+                window.location.href = '../pages/home.html'
+            }
+        })
+    }
+}
+
+
+window.onload = () => {
+    button.addEventListener('click', validarLogin)
+}
