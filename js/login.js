1eda723edbb81013d1d09d20fd88f60fdb268071
JESSICA PEREIRA DOS SANTOS <23111734@CFP127ALUNO.LOCAL>
2024-02-22 10:02:57 -0300

    adicionar login funcional

--- js/login.js
+++ js/login.js
@@ -3,25 +3,28 @@ const button = document.getElementById('login');
 async function validarLogin() {
     const email = document.getElementById('email').value;
     const senha = document.getElementById('senha').value;
+    let logado = false
 
-    if (email === '' || senha === '') {
-        alert('preencha os campos corretamente')
-    } else {
+    try {
         const users = await fetch('http://localhost:5080/usuario')
         const listUsers = await users.json()
-
-        console.log(listUsers)
-
         listUsers.forEach((user) => {
             if (email === user.email && senha === user.senha) {
-                alert('Usuario logado com sucesso !!!')
+                logado = true
+                localStorage.setItem("idusuario", user.id)
                 window.location.href = '../pages/home.html'
             }
+            console.log(user.email)
+            console.log(user.senha)
+            console.log(user.id)
         })
+
+        if (!logado) {
+            alert('login inválido')
+        }
+    } catch (error) {
+        console.log(error)
     }
+
 }
 
-
-window.onload = () => {
-    button.addEventListener('click', validarLogin)
-}
