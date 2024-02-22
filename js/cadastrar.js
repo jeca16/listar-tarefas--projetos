ea88dc6a5a6f6d11d11d0f3ca804347999aee91e
JESSICA PEREIRA DOS SANTOS <23111734@CFP127ALUNO.LOCAL>
2024-02-22 09:51:54 -0300

    adicionar cadastro funcional

---
+++ js/cadastrar.js
@@ -0,0 +1,34 @@
+'use strict'
+
+async function cadastrarUsuario() {
+    const nome = document.getElementById('nome').value
+    const email = document.getElementById('email').value
+    const senha = document . getElementById ('senha').value
+    const confirmarSenha = document.getElementById('confirmarSenha').value
+
+    if (nome == '' || email == '' || senha == '' || confirmarSenha == '') {
+        alert('preencha os campos devidamente!')
+
+    }else{
+        if (confirmarSenha != senha) {
+            alert('As senhas informadas não são iguais')
+        }
+        try {
+            const novoUsuario = {
+                nome: nome,
+                email: email,
+                senha: senha
+            }
+            await fetch('http://localhost:5080/usuario', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(novoUsuario)
+            })
+            window.location.href = '../login.html'
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
